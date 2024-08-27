b57398cd3c2a26b35a50935dbeaa1778ff8bf1b8
n0vella <n0vella@outlook.com>
2024-08-27 14:32:18 +0200

    tip button

--- src/Icons.tsx
+++ src/Icons.tsx
@@ -40,3 +40,14 @@ export const github = (
     <path d="M9 19c-4.3 1.4 -4.3 -2.5 -6 -3m12 5v-3.5c0 -1 .1 -1.4 -.5 -2c2.8 -.3 5.5 -1.4 5.5 -6a4.6 4.6 0 0 0 -1.3 -3.2a4.2 4.2 0 0 0 -.1 -3.2s-1.1 -.3 -3.5 1.3a12.3 12.3 0 0 0 -6.2 0c-2.4 -1.6 -3.5 -1.3 -3.5 -1.3a4.2 4.2 0 0 0 -.1 3.2a4.6 4.6 0 0 0 -1.3 3.2c0 4.6 2.7 5.7 5.5 6c-.6 .6 -.6 1.2 -.5 2v3.5" />
   </svg>
 )
+
+export const tipJar = (
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round" className="icon icon-tabler icons-tabler-outline icon-tabler-tip-jar">
+    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+    <path d="M14 10h-2.5a1.5 1.5 0 0 0 0 3h1a1.5 1.5 0 0 1 0 3h-2.5" />
+    <path d="M12 9v1" />
+    <path d="M12 16v1" />
+    <path d="M17 4v1.882c0 .685 .387 1.312 1 1.618s1 .933 1 1.618v8.882a3 3 0 0 1 -3 3h-8a3 3 0 0 1 -3 -3v-8.882c0 -.685 .387 -1.312 1 -1.618s1 -.933 1 -1.618v-1.882" />
+    <path d="M6 4h12z" />
+  </svg>
+)
--- src/app.tsx
+++ src/app.tsx
@@ -2,6 +2,7 @@ import Router, { CustomHistory, Route, route } from "preact-router";
 import Cardo from "./pages/Cardo";
 import { useEffect } from "preact/hooks";
 import { createHashHistory } from 'history';
+import * as icons from './Icons'
 
 
 function Redirect(to: string) {
@@ -14,7 +15,24 @@ export function App() {
 
   return (
     <div className='w-full h-screen bg-zinc-900 pt-10 font-sans overflow-y-auto scroll-smooth'>
-      <div className='absolute top-0 w-full h-12 bg-zinc-950 flex justify-end px-3 py-1'>
+      <div className='absolute top-0 w-full h-12 bg-zinc-950 flex justify-between px-3 py-1'>
+        <div className='group'>
+
+          <div className='bg-yellow-400 items-center rounded-md py-1 px-2 flex group-hover:hidden'>
+            <span className='w-8'>{icons.tipJar}</span>
+            <p className='font-medium'>¿DO YOU LIKE THIS?</p>
+          </div>
+
+          <div className='gap-1 hidden group-hover:flex'>
+          <a href="https://www.buymeacoffee.com/n0vella" target="_blank">
+            <img src="https://cdn.buymeacoffee.com/buttons/v2/default-yellow.png" alt="Buy Me A Coffee" className="h-10" />
+          </a>
+          <a href="https://www.paypal.com/paypalme/adriannovella" target="_blank">
+            <img src="https://www.paypalobjects.com/webstatic/icon/pp196.png" alt="Paypal" className="h-10 rounded-md" />
+          </a>
+        </div>
+        </div>
+
         <a href='https://github.com/n0vella' target='_blank'
           className=' contents'
         >
